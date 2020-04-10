a1eb0350f1ce75ccc0488167822da6a3c3cd7598
meisben <ben.money+github@gmail.com>
2020-04-10 15:56:06 +0100

    Added view and template for detailed blog post as well as CSS

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,4 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static') # note commented out due to stack overflow answer https://stackoverflow.com/questions/25913849/django-static-file-not-loading
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
